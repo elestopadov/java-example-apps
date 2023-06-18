04e676ac83cc4ce16129ad8449c5131b3dcfc4b2
Administrator <admin@example.com>
2023-06-18 12:16:30 +0400

    Added new console application java

---
+++ app-console/com/evg/AppConsole.java
@@ -0,0 +1,15 @@
+package com.evg;
+
+/** 
+ * This is simple class 'AppConsole' with method <b>main</b>.
+ * @autor Evg
+ * @version 0.1
+*/
+class AppConsole {
+  /**
+  * This method: main
+  */
+  public static void main(String[] args) {
+    System.out.println("Welcome to App-Console.");
+  }
+}
