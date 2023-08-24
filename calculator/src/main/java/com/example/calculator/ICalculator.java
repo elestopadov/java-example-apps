7383cbca47205f3ffe2ec3b33033725dddffe98a
elestopadov <75382023+elestopadov@users.noreply.github.com>
2023-08-24 23:15:17 +0400

    Update ICalculator.java
    added new comment for example javadoc

--- calculator/src/main/java/com/example/calculator/ICalculator.java
+++ calculator/src/main/java/com/example/calculator/ICalculator.java
@@ -1,13 +1,30 @@
 package com.example.calculator;
 
+/** 
+ * Class Description of ICalculator.
+ * @author Evg
+ * @version 0.1
+*/
+
 public interface ICalculator {
+    /**
+     * Description of method sum(). 
+    */
     int sum(int a, int b);
-
+    /**
+     * Description of method subtraction(). 
+    */
     int subtraction(int a, int b);
-
+    /**
+     * Description of method multiplication(). 
+    */
     int multiplication(int a, int b);
-
+    /**
+     * Description of method divison(). 
+    */
     int divison(int a, int b) throws Exception;
-
+    /**
+     * Description of method equalIntegers(). 
+    */
     boolean equalIntegers(int a, int b);
 }
